1b65c2c8031ab48c5af5ff4a2f86d99071b312c3
Maxim Polyakov <max.senia.poliak@gmail.com>
2019-08-21 17:30:08 +0300

    Fix generated files header
    Signed-off-by: Maxim Polyakov <max.senia.poliak@gmail.com>

--- pch-pads-parser.go
+++ pch-pads-parser.go
@@ -170,8 +170,6 @@ func HdrInfoAdd(f *os.File) {
 	f.WriteString(`/*
  * This file is part of the coreboot project.
  *
- * Copyright (C) 2019 Maxim Polyakov <max.senia.poliak@gmail.com>
- *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; version 2 of
