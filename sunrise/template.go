b432f0ed518718efa043da56dc76b53aa965cf2d
Maxim Polyakov <max.senia.poliak@gmail.com>
2020-03-20 23:30:41 +0300

    sunrise/template: add a template for filtering keywords
    Signed-off-by: Maxim Polyakov <max.senia.poliak@gmail.com>

---
+++ sunrise/template.go
@@ -0,0 +1,19 @@
+package sunrise
+
+import (
+	"strings"
+)
+
+// keywordCheck - This function is used to filter parsed lines of the configuration file and
+//                returns true if the keyword is contained in the line.
+// line      : string from the configuration file
+func KeywordCheck(line string) bool {
+	for _, keyword := range []string{
+		"GPP_", "GPD",
+	} {
+		if strings.Contains(line, keyword) {
+			return true
+		}
+	}
+	return false
+}
